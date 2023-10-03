660b1911acd81b681dee51c6a5c98bb011b7d931
abbas_alkaabi <abbasalkabbi2003@gmail.com>
2023-10-04 02:42:02 +0300

    Add -> db

--- app/Http/Controllers/StackPageController.php
+++ app/Http/Controllers/StackPageController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\phone;
 class StackPageController extends Controller
 {
     public function  index(){
-        return view('home');
+        $phones =phone::all();
+        return view('home',["phones"=>$phones]);
     }
     public function  about(){
         return view('about');
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,4 +2,11 @@
 @section('title','home')
 @section('content')
     <h1>Home </h1>
+    <ul>
+        @foreach ($phones as $item)
+        <li>
+            {{$item->name}}
+        </li>
+        @endforeach
+    </ul>
 @endsection
