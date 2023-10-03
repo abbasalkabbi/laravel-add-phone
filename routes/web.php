338311ceb3d3de3916f81c1658b03991e687e3ed
abbas_alkaabi <abbasalkabbi2003@gmail.com>
2023-10-04 02:12:09 +0300

    Add => StackPage

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StackPageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', [StackPageController::class,'index'])->name('home');
+Route::get('/about', [StackPageController::class,'about'])->name('about');
