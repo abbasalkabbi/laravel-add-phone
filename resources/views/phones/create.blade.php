3be780f5d601cc33262dbc1c20c3f1ab72a14b90
abbas_alkaabi <abbasalkabbi2003@gmail.com>
2023-10-04 07:10:11 +0300

    Style->create

--- resources/views/phones/create.blade.php
+++ resources/views/phones/create.blade.php
@@ -3,11 +3,13 @@
 @section('content') 
 <style>
     form{
-        margin:auto;width:40%;padding:10px;
+        margin:auto;
+        width:400px;
+        padding:10px;
     }
 </style>
 <h2 class="text-center">Add New Phone</h2>
-    <form class="card "  >
+    <form class="card "  method="POST">
         @csrf
         <div class="mb-3">
             <label for="name-phone" class="form-label">Name Of Phone</label>
@@ -19,6 +21,9 @@
             <input type="text" class="form-control" id="price-phone" name="price-phone">
             <div class="form-text text-danger ">Price Error</div>
         </div>
-        <button type="submit" class="btn btn-primary">Save</button>
+        <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+            <button type="submit" class="btn btn-primary">Save</button>
+            <button type="button" class="btn btn-danger">canel</button>
+        </div>
     </form>
 @endsection
